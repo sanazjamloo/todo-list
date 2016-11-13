77f35455773357f62a35877226156f7e842ea78a
Sanaz <sanaz.jamloo@gmail.com>
2016-11-12 22:05:30 -0800

    Add get method in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,36 @@
 (function(){
-  angular.module('todo', []).controller('MainCtrl', function() {
+  angular.module('todo', []).controller('MainCtrl', function($http) {
+
+        var self = this;
+
+    $http.get('/todos')  // make a GET request to /todos
+      .then(function(response) { // call this function when a response is received
+        self.todos = response.data.todos; // save the returned json to self.todos
+      })
+      .catch(function(err) { // catch the error if one happens
+        console.log(err)
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
   });
